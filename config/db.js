e7f494ec9b3959c36f20169914f1440e2b421a0c
Neo-Info <21brgames@gmail.com>
2021-12-22 18:54:51 -0300

    Neo-Info

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 if(process.env.NODE_ENV == 'production'){
     module.exports = {mongoURI: 'mongodb+srv://neoinfo:Sc6rTjmkx4V6FZop@cluster0.fdcam.mongodb.net/blogapp?retryWrites=true&w=majority'}
 }else{
-    module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
+    //module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
+    module.exports = {mongoURI: 'mongodb+srv://neoinfo:Sc6rTjmkx4V6FZop@cluster0.fdcam.mongodb.net/blogapp?retryWrites=true&w=majority'}
 }

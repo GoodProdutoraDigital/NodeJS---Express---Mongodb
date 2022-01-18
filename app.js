92725e690d708d9e6231e743e14439c91ec5c7be
Neo-Info <21brgames@gmail.com>
2022-01-18 14:19:58 -0300

    PORT

--- app.js
+++ app.js
@@ -147,7 +147,7 @@
     app.use('/user', user)
 
 //outros
-    const PORT = process.env.PORT || 3000
+    const PORT = process.env.PORT || 3333
     app.listen(PORT, () => {
-        console.log('executando servidor http://localhost:3000')
+        console.log('executando servidor http://localhost:' + PORT)
     })

5b9d090bc963fadf47244d64a86d430cdf8dbad4
Neo-Info <21brgames@gmail.com>
2022-01-18 14:23:10 -0300

    URI

--- config/db.js
+++ config/db.js
@@ -1,2 +1,5 @@
-module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
-//module.exports = {mongoURI: 'mongodb+srv://user:pass@cluster0.fdcam.mongodb.net/blogapp?retryWrites=true&w=majority'}
+if(process.env.NODE_ENV == 'production'){
+    module.exports = {mongoURI: 'mongodb+srv://user:password@cluster0.fdcam.mongodb.net/blogapp?retryWrites=true&w=majority'}
+}else{
+    module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
+}
